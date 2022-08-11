5b21047b5510bb95eb091493bf79084fa6594dec
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2022-08-11 16:00:12 +0530

    Update __init__.py

--- tobrot/bot_theme/themes/__init__.py
+++ tobrot/bot_theme/themes/__init__.py
@@ -8,7 +8,6 @@ AVAILABLE_THEMES = {'fx-optimised-theme': fx_optimised, 'fx-minimal-theme': fx_m
 def BotTheme(user_id_):
 
     theme_ = USER_THEMES.get(str(user_id_), BOT_THEME)
-    theme_ = USER_THEMES.get(user_id_, BOT_THEME)
     if theme_ in AVAILABLE_THEMES.keys():
         return (AVAILABLE_THEMES.get(theme_)).TXStyle()
     elif theme_ == "fx-random-theme":
