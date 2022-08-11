35c1237d2ea37f5d96c55a7e7824042958af32bc
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2022-08-11 15:59:04 +0530

    Update __init__.py

--- tobrot/bot_theme/themes/__init__.py
+++ tobrot/bot_theme/themes/__init__.py
@@ -3,17 +3,14 @@ from random import choice
 from tobrot import BOT_THEME, USER_THEMES, LOGGER
 from tobrot.bot_theme.themes import fx_optimised, fx_minimal
 
-AVAILABLE_THEMES = {'fx-optimised-theme': fx_optimised, 'fx-minimal-theme': fx_minimal.__name__}
+AVAILABLE_THEMES = {'fx-optimised-theme': fx_optimised, 'fx-minimal-theme': fx_minimal}
 
 def BotTheme(user_id_):
 
     theme_ = USER_THEMES.get(str(user_id_), BOT_THEME)
-    LOGGER.info(theme_)
-    LOGGER.info(user_id_)
-    if theme_ == "fx-optimised-theme":
-        return fx_optimised.TXStyle()
-    elif theme_ == "fx-minimal-theme":
-        return fx_minimal.TXStyle()
+    theme_ = USER_THEMES.get(user_id_, BOT_THEME)
+    if theme_ in AVAILABLE_THEMES.keys():
+        return (AVAILABLE_THEMES.get(theme_)).TXStyle()
     elif theme_ == "fx-random-theme":
         rantheme = choice(list(AVAILABLE_THEMES.values()))
         LOGGER.info(f"Random Theme Choosen : {rantheme}")
